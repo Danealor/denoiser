5e24aa9148f6205d916f1016455e15c27243834a
danealor <iamblonder@gmail.com>
2021-04-15 01:54:00 -0400

    DSP and loss to tensorflow

--- denoiser/dsp.py
+++ denoiser/dsp.py
@@ -6,26 +6,33 @@
 # author: adefossez
 
 import numpy as np
-import torch
-from torch.nn import functional as F
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.experimental.numpy import sinc
+
+def log1p10(x):
+    return tf.math.log1p(x) / np.log(10)
+
+def exp10m1(x):
+    return tf.math.expm1(x * np.log(10))
 
 
 def hz_to_mel(f):
-    return 2595 * np.log10(1 + f / 700)
-
+    return 2595 * log1p10(f / 700)
 
 def mel_to_hz(m):
-    return 700 * (10**(m / 2595) - 1)
+    return 700 * exp10m1(m / 2595)
 
 
+@tf.function
 def mel_frequencies(n_mels, fmin, fmax):
     low = hz_to_mel(fmin)
     high = hz_to_mel(fmax)
-    mels = np.linspace(low, high, n_mels)
+    mels = tf.linspace(low, high, n_mels)
     return mel_to_hz(mels)
 
 
-class LowPassFilters(torch.nn.Module):
+class LowPassFilters(layers.Layer):
     """
     Bank of low pass filters.
 
@@ -40,25 +47,27 @@ class LowPassFilters(torch.nn.Module):
         - Output: `(F, *, T` with `F` the len of `cutoffs`.
     """
 
-    def __init__(self, cutoffs: list, width: int = None):
-        super().__init__()
+    def __init__(self, cutoffs, width: int = None, **kwargs):
+        super().__init__(**kwargs)
         self.cutoffs = cutoffs
         if width is None:
             width = int(2 / min(cutoffs))
         self.width = width
-        window = torch.hamming_window(2 * width + 1, periodic=False)
-        t = np.arange(-width, width + 1, dtype=np.float32)
-        filters = []
-        for cutoff in cutoffs:
-            sinc = torch.from_numpy(np.sinc(2 * cutoff * t))
-            filters.append(2 * cutoff * sinc * window)
-        self.register_buffer("filters", torch.stack(filters).unsqueeze(1))
+        window = tf.signal.hamming_window(2 * width + 1, periodic=False)
+        t = tf.range(-width, width + 1, dtype=tf.float32)
+        sincs = sinc(2 * cutoffs[:,tf.newaxis] * t[tf.newaxis,:])
+        filters = 2 * cutoffs[:,tf.newaxis] * sincs * window[tf.newaxis,:]
+        self.filters = tf.transpose(filters)[:,tf.newaxis,:]
 
-    def forward(self, input):
-        *others, t = input.shape
-        input = input.view(-1, 1, t)
-        out = F.conv1d(input, self.filters, padding=self.width)
-        return out.permute(1, 0, 2).reshape(-1, *others, t)
+    def get_config(self):
+        config = super().get_config()
+        config.update({"cutoffs": self.cutoffs})
+        config.update({"width": self.width})
+        return config
+
+    def call(self, input):
+        out = tf.nn.conv1d(input, self.filters, stride=1, padding='SAME')
+        return tf.transpose(out, (2,0,1))[...,tf.newaxis]
 
     def __repr__(self):
         return "LossPassFilters(width={},cutoffs={})".format(self.width, self.cutoffs)
--- denoiser/stft_loss.py
+++ denoiser/stft_loss.py
@@ -10,8 +10,8 @@
 
 """STFT-based Loss modules."""
 
-import torch
-import torch.nn.functional as F
+import tensorflow as tf
+from tensorflow.keras import layers, losses
 
 
 def stft(x, fft_size, hop_size, win_length, window):
@@ -25,22 +25,20 @@ def stft(x, fft_size, hop_size, win_length, window):
     Returns:
         Tensor: Magnitude spectrogram (B, #frames, fft_size // 2 + 1).
     """
-    x_stft = torch.stft(x, fft_size, hop_size, win_length, window)
-    real = x_stft[..., 0]
-    imag = x_stft[..., 1]
+    x_stft = tf.signal.stft(x, win_length, hop_size, fft_size, window)
 
     # NOTE(kan-bayashi): clamp is needed to avoid nan or inf
-    return torch.sqrt(torch.clamp(real ** 2 + imag ** 2, min=1e-7)).transpose(2, 1)
+    return tf.clip_by_value(tf.abs(x_stft), 1e-7, tf.dtypes.float32.max)
 
 
-class SpectralConvergengeLoss(torch.nn.Module):
-    """Spectral convergence loss module."""
+class SpectralConvergenceLoss(layers.Layer):
+    """Spectral convergence loss layer."""
 
-    def __init__(self):
-        """Initilize spectral convergence loss module."""
-        super(SpectralConvergengeLoss, self).__init__()
+    def __init__(self, **kwargs):
+        """Initilize spectral convergence loss layer."""
+        super().__init__(**kwargs)
 
-    def forward(self, x_mag, y_mag):
+    def call(self, x_mag, y_mag):
         """Calculate forward propagation.
         Args:
             x_mag (Tensor): Magnitude spectrogram of predicted signal (B, #frames, #freq_bins).
@@ -48,17 +46,17 @@ class SpectralConvergengeLoss(torch.nn.Module):
         Returns:
             Tensor: Spectral convergence loss value.
         """
-        return torch.norm(y_mag - x_mag, p="fro") / torch.norm(y_mag, p="fro")
+        return tf.norm(y_mag - x_mag, ord="fro") / tf.norm(y_mag, ord="fro")
 
 
-class LogSTFTMagnitudeLoss(torch.nn.Module):
+class LogSTFTMagnitudeLoss(layers.Layer):
     """Log STFT magnitude loss module."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Initilize los STFT magnitude loss module."""
-        super(LogSTFTMagnitudeLoss, self).__init__()
+        super().__init__(**kwargs)
 
-    def forward(self, x_mag, y_mag):
+    def call(self, x_mag, y_mag):
         """Calculate forward propagation.
         Args:
             x_mag (Tensor): Magnitude spectrogram of predicted signal (B, #frames, #freq_bins).
@@ -66,23 +64,36 @@ class LogSTFTMagnitudeLoss(torch.nn.Module):
         Returns:
             Tensor: Log STFT magnitude loss value.
         """
-        return F.l1_loss(torch.log(y_mag), torch.log(x_mag))
+        return losses.MAE(tf.log(y_mag), tf.log(x_mag))
 
 
-class STFTLoss(torch.nn.Module):
+class STFTLoss(layers.Layer):
     """STFT loss module."""
 
     def __init__(self, fft_size=1024, shift_size=120, win_length=600, window="hann_window"):
         """Initialize STFT loss module."""
-        super(STFTLoss, self).__init__()
+        super().__init__(**kwargs)
         self.fft_size = fft_size
         self.shift_size = shift_size
         self.win_length = win_length
-        self.register_buffer("window", getattr(torch, window)(win_length))
-        self.spectral_convergenge_loss = SpectralConvergengeLoss()
+        self.window = window
+        
+        self.spectral_convergence_loss = SpectralConvergenceLoss()
         self.log_stft_magnitude_loss = LogSTFTMagnitudeLoss()
+        if hasattr(tf.signal, window):
+            self.window_fn = getattr(tf.signal, window)
+        else:
+            raise ValueError(f"Invalid window function '{window}'.")
 
-    def forward(self, x, y):
+    def get_config(self):
+        config = super().get_config()
+        config.update({"fft_size": self.fft_size})
+        config.update({"shift_size": self.shift_size})
+        config.update({"win_length": self.win_length})
+        config.update({"window": self.window})
+        return config
+
+    def call(self, x, y):
         """Calculate forward propagation.
         Args:
             x (Tensor): Predicted signal (B, T).
@@ -91,39 +102,50 @@ class STFTLoss(torch.nn.Module):
             Tensor: Spectral convergence loss value.
             Tensor: Log STFT magnitude loss value.
         """
-        x_mag = stft(x, self.fft_size, self.shift_size, self.win_length, self.window)
-        y_mag = stft(y, self.fft_size, self.shift_size, self.win_length, self.window)
-        sc_loss = self.spectral_convergenge_loss(x_mag, y_mag)
+        x_mag = stft(x, self.fft_size, self.shift_size, self.win_length, self.window_fn)
+        y_mag = stft(y, self.fft_size, self.shift_size, self.win_length, self.window_fn)
+        sc_loss = self.spectral_convergence_loss(x_mag, y_mag)
         mag_loss = self.log_stft_magnitude_loss(x_mag, y_mag)
 
         return sc_loss, mag_loss
 
 
-class MultiResolutionSTFTLoss(torch.nn.Module):
+class MultiResolutionSTFTLoss(layers.Layer):
     """Multi resolution STFT loss module."""
 
     def __init__(self,
                  fft_sizes=[1024, 2048, 512],
                  hop_sizes=[120, 240, 50],
                  win_lengths=[600, 1200, 240],
-                 window="hann_window", factor_sc=0.1, factor_mag=0.1):
+                 window="hann_window", **kwargs):
         """Initialize Multi resolution STFT loss module.
         Args:
             fft_sizes (list): List of FFT sizes.
             hop_sizes (list): List of hop sizes.
             win_lengths (list): List of window lengths.
             window (str): Window function type.
-            factor (float): a balancing factor across different losses.
         """
-        super(MultiResolutionSTFTLoss, self).__init__()
-        assert len(fft_sizes) == len(hop_sizes) == len(win_lengths)
-        self.stft_losses = torch.nn.ModuleList()
-        for fs, ss, wl in zip(fft_sizes, hop_sizes, win_lengths):
-            self.stft_losses += [STFTLoss(fs, ss, wl, window)]
+        super().__init__(**kwargs)
+        self.fft_sizes = fft_sizes
+        self.hop_size = hop_sizes
+        self.win_lengths = win_lengths
+        self.window = window
         self.factor_sc = factor_sc
         self.factor_mag = factor_mag
+        assert len(fft_sizes) == len(hop_sizes) == len(win_lengths)
 
-    def forward(self, x, y):
+        self.stft_losses = [STFTLoss(fs, ss, wl, window) for fs, ss, wl 
+                            in zip(fft_sizes, hop_sizes, win_lengths)]
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"fft_sizes": self.fft_sizes})
+        config.update({"hop_sizes": self.hop_sizes})
+        config.update({"win_lengths": self.win_lengths})
+        config.update({"window": self.window})
+        return config
+
+    def call(self, x, y):
         """Calculate forward propagation.
         Args:
             x (Tensor): Predicted signal (B, T).
@@ -141,4 +163,59 @@ class MultiResolutionSTFTLoss(torch.nn.Module):
         sc_loss /= len(self.stft_losses)
         mag_loss /= len(self.stft_losses)
 
-        return self.factor_sc*sc_loss, self.factor_mag*mag_loss
+        return sc_loss, mag_loss
+
+
+class TotalMultiResolutionSTFTLoss(losses.Loss):
+    """Multi resolution STFT loss module."""
+
+    def __init__(self,
+                 fft_sizes=[1024, 2048, 512],
+                 hop_sizes=[120, 240, 50],
+                 win_lengths=[600, 1200, 240],
+                 window="hann_window", factor_sc=0.1, factor_mag=0.1, **kwargs):
+        """Initialize Multi resolution STFT loss module.
+        Args:
+            fft_sizes (list): List of FFT sizes.
+            hop_sizes (list): List of hop sizes.
+            win_lengths (list): List of window lengths.
+            window (str): Window function type.
+            factor (float): a balancing factor across different losses.
+        """
+        super().__init__(**kwargs)
+        self.fft_sizes = fft_sizes
+        self.hop_size = hop_sizes
+        self.win_lengths = win_lengths
+        self.window = window
+        self.factor_sc = factor_sc
+        self.factor_mag = factor_mag
+
+        self.mrstft_loss = MultiResolutionSTFTLoss(fft_sizes, hop_sizes, win_lengths, window)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"fft_sizes": self.fft_sizes})
+        config.update({"hop_sizes": self.hop_sizes})
+        config.update({"win_lengths": self.win_lengths})
+        config.update({"window": self.window})
+        config.update({"factor_sc": self.factor_sc})
+        config.update({"factor_mag": self.factor_mag})
+        return config
+
+    def call(self, y_true, y_pred):
+        """Calculate forward propagation.
+        Args:
+            x (Tensor): Predicted signal (B, T).
+            y (Tensor): Groundtruth signal (B, T).
+        Returns:
+            Tensor: Multi resolution spectral convergence loss value.
+            Tensor: Multi resolution log STFT magnitude loss value.
+        """
+        # Combine to mono if not already and squeeze out channel dim
+        y_true, y_pred = map(lambda y: tf.reduce_mean(y, axis=-1, keepdims=False), y_true, y_pred)
+
+        # Calculate sc and mag loss
+        sc_loss, mag_loss = self.mrstft_loss(y_pred, y_true)
+
+        # Calculate weighted total loss
+        return self.factor_sc*sc_loss + self.factor_mag*mag_loss
